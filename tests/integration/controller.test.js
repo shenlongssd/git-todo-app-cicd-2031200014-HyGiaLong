5ac5ca79f72af090dde226cc87818d7a33e0f3c1
shenlongssd <long.hy.cit20@eiu.edu.vn>
2026-07-21 08:11:19 +0700

    Test: Implement integration tests for Controller-Model interaction

--- tests/integration/controller.test.js
+++ tests/integration/controller.test.js
@@ -20,16 +20,33 @@ describe('Controller-Service Integration Tests', () => {
     });
 
     test('handleAddTodo should call service.addTodo and update the model', () => {
-        // TODO: Call the controller's handleAddTodo method with some test text.
-        // Then, get the list of todos directly from the service.
-        // Assert that the service's todos array has a length of 1.
-        // Assert that the text of the first todo in the service matches the input.
+        // 1. Gọi phương thức handleAddTodo của controller với chuỗi văn bản test
+        const todoText = 'Test Integration Task';
+        controller.handleAddTodo(todoText);
+
+        // 2. Lấy danh sách todos trực tiếp từ service
+        const todos = service.getTodos();
+
+        // 3. Khẳng định (Assert) mảng todos của service có độ dài là 1
+        expect(todos.length).toBe(1);
+
+        // 4. Khẳng định text của todo đầu tiên trùng khớp với input
+        expect(todos[0].text || todos[0].title).toBe(todoText);
     });
 
     test('handleRemoveTodo should call service.removeTodo and update the model', () => {
-        // TODO: First, directly add a todo to the service.
-        // Get the ID of the new todo.
-        // Call the controller's handleRemoveTodo method with that ID.
-        // Assert that the service's todos array is now empty.
+        // 1. Thêm trực tiếp một todo vào service
+        service.addTodo('Task to be removed');
+        
+        // 2. Lấy ID của todo vừa tạo
+        const todosBefore = service.getTodos();
+        const todoId = todosBefore[0].id;
+
+        // 3. Gọi phương thức handleRemoveTodo của controller với ID đó
+        controller.handleRemoveTodo(todoId);
+
+        // 4. Khẳng định mảng todos của service giờ đây đã trống (độ dài bằng 0)
+        const todosAfter = service.getTodos();
+        expect(todosAfter.length).toBe(0);
     });
 });
